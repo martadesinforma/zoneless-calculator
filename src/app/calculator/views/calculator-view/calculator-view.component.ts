aa2b3a794aa694486aba9e3178e27df4482d1ee5
Marta Ocana <martacyta2021@gmail.com>
2024-10-07 13:51:00 +0200

    create calculator-view component

---
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -0,0 +1,13 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator-view',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './calculator-view.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class CalculatorViewComponent { }
