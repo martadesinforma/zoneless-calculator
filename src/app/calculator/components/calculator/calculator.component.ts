c31ad6c23cd4ab83f3f1baa9c00b93bcb9004fc3
Marta Ocana <martacyta2021@gmail.com>
2024-10-07 13:58:34 +0200

    create calculator component

---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,13 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CalculatorComponent { }
