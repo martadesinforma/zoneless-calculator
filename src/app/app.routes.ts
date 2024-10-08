115a909a965620b86fe07f2b64bd6ff5ba4aa05d
Marta Ocana <martacyta2021@gmail.com>
2024-10-08 17:58:46 +0200

    modify app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'calculator',
+    loadComponent: () => import('./calculator/views/calculator-view/calculator-view.component'),
+  },
+  {
+    path: '**', /* cualquier ruta que no haya sido previamente definida en las rutas de tu aplicación */
+    redirectTo: 'calculator',
+  }
+];
