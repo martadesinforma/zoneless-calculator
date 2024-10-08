e6980098439674b24a0bf8ddec115c65ceb6fd8f
Marta Ocana <martacyta2021@gmail.com>
2024-10-08 17:57:20 +0200

    modify calculator component

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,13 +1,15 @@
 
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 @Component({
   selector: 'calculator',
   standalone: true,
   imports: [
-
+    CalculatorButtonComponent
   ],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator.component.css',
 })
 export class CalculatorComponent { }
